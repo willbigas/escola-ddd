853ef2b45d9a9ef8079326b81915fdfccc6c4df3
William Bigas Mauro <williambmauro@hotmail.com>
2022-02-28 18:06:55 -0300

    Adicionado FabricaDeAluno

--- src/main/java/Aluno.java
+++ src/main/java/Aluno.java
@@ -10,6 +10,12 @@ public class Aluno {
 
     private List<Telefone> telefones = new ArrayList<>();
 
+    public Aluno(CPF cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
     public void adicionarTelefone(String ddd, String numero) {
         this.telefones.add(new Telefone(ddd, numero));
     }
